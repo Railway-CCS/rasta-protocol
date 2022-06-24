d10e6534e474e25973100cf48684ecb540625d22
Benedikt Schenkel <benedikt.schenkel@student.hpi.de>
2022-06-24 13:26:33 +0000

    add precompile flag for logging

--- src/rasta/c/logging.c
+++ src/rasta/c/logging.c
@@ -10,14 +10,21 @@
 #include <syscall.h>
 #include <logging.h>
 
+#define UNUSED(x) (void)(x)
+
 /**
  * logs a string to the console
  * @param message the message that will be logged
  */
-void log_to_console(const char * message){
+void log_to_console(const char *message)
+{
+#ifdef logging
     printf("%s", message);
     // flush buffer
     fflush(stdout);
+#else
+    UNUSED(message);
+#endif
 }
 
 /**
@@ -25,16 +32,20 @@ void log_to_console(const char * message){
  * @param message the message that will be logged
  * @param log_file the file where the @p message is appended
  */
-void log_to_file(const char * message, const char * log_file){
+void log_to_file(const char *message, const char *log_file)
+{
+#ifdef logging
     // check if path to log file has been set
-    if (log_file == NULL){
+    if (log_file == NULL)
+    {
         perror("Log file not specified\n");
     }
 
     // open file in append mode
-    FILE * pFile = fopen(log_file, "a");
+    FILE *pFile = fopen(log_file, "a");
 
-    if (pFile == NULL){
+    if (pFile == NULL)
+    {
         // error while opening file
         perror("Could not open log file\n");
     }
@@ -43,6 +54,10 @@ void log_to_file(const char * message, const char * log_file){
     fprintf(pFile, "%s", message);
 
     fclose(pFile);
+#else
+    UNUSED(message);
+    UNUSED(log_file);
+#endif
 }
 
 /**
@@ -53,10 +68,12 @@ void log_to_file(const char * message, const char * log_file){
  * @param msg_str the log message
  * @return the log message string
  */
-char * get_log_message_string(log_level max_log_level, log_level level, char * location, char * msg_str){
-
+char *get_log_message_string(log_level max_log_level, log_level level, char *location, char *msg_str)
+{
+#ifdef logging
     // check if maximum log level allows this message
-    if (level > max_log_level){
+    if (level > max_log_level)
+    {
         // not allowed, return
         return NULL;
     }
@@ -64,7 +81,7 @@ char * get_log_message_string(log_level max_log_level, log_level level, char * l
     // generate timestamp
     time_t current_time = time(NULL);
     struct tm tt;
-    struct tm * time_info = localtime_r(&current_time, &tt);
+    struct tm *time_info = localtime_r(&current_time, &tt);
     char timestamp[30];
     char timestamp2[60];
 
@@ -74,24 +91,24 @@ char * get_log_message_string(log_level max_log_level, log_level level, char * l
     gettimeofday(&tv, NULL);
 
     unsigned long long millisecondsSinceEpoch =
-            (unsigned long long)(tv.tv_sec) * 1000 +
-            (unsigned long long)(tv.tv_usec) / 1000;
-
+        (unsigned long long)(tv.tv_sec) * 1000 +
+        (unsigned long long)(tv.tv_usec) / 1000;
 
     // generate log level string
     char level_str[30];
-    switch (level){
-        case LOG_LEVEL_DEBUG:
-            rstrcpy(level_str, "DEBUG");
-            break;
-        case LOG_LEVEL_ERROR:
-            rstrcpy(level_str, "ERROR");
-            break;
-        case LOG_LEVEL_INFO:
-            rstrcpy(level_str, "INFO");
-            break;
-        default:
-            perror("invalid log level\n");
+    switch (level)
+    {
+    case LOG_LEVEL_DEBUG:
+        rstrcpy(level_str, "DEBUG");
+        break;
+    case LOG_LEVEL_ERROR:
+        rstrcpy(level_str, "ERROR");
+        break;
+    case LOG_LEVEL_INFO:
+        rstrcpy(level_str, "INFO");
+        break;
+    default:
+        perror("invalid log level\n");
     }
 
     // format timestamp
@@ -100,13 +117,21 @@ char * get_log_message_string(log_level max_log_level, log_level level, char * l
     // add milliseconds to timestamp
     sprintf(timestamp2, "%s (Epoch time: %llu)", timestamp, millisecondsSinceEpoch);
 
-    char *  msg_string = rmalloc(LOGGER_MAX_MSG_SIZE);
+    char *msg_string = rmalloc(LOGGER_MAX_MSG_SIZE);
     sprintf(msg_string, LOG_FORMAT, timestamp2, level_str, location, msg_str);
 
     return msg_string;
+#else
+    UNUSED(max_log_level);
+    UNUSED(level);
+    UNUSED(location);
+    UNUSED(msg_str);
+#endif
+    return NULL;
 }
 
-struct logger_t logger_init(log_level max_log_level, logger_type type){
+struct logger_t logger_init(log_level max_log_level, logger_type type)
+{
     struct logger_t logger;
 
     logger.type = type;
@@ -119,12 +144,21 @@ struct logger_t logger_init(log_level max_log_level, logger_type type){
     return logger;
 }
 
-void logger_set_log_file(struct logger_t* logger, char * path){
+void logger_set_log_file(struct logger_t *logger, char *path)
+{
+#ifdef logging
     logger->log_file = path;
+#else
+    UNUSED(logger);
+    UNUSED(path);
+#endif
 }
 
-void logger_log(struct logger_t * logger, log_level level, char* location, char* format, ...){
-    if (logger == NULL || logger->max_log_level == LOG_LEVEL_NONE){
+void logger_log(struct logger_t *logger, log_level level, char *location, char *format, ...)
+{
+#ifdef logging
+    if (logger == NULL || logger->max_log_level == LOG_LEVEL_NONE)
+    {
         return;
     }
 
@@ -136,36 +170,53 @@ void logger_log(struct logger_t * logger, log_level level, char* location, char*
     va_end(args);
 
     log_level max_lvl = logger->max_log_level;
-    char * msg = get_log_message_string(max_lvl, level, location, message);
-    if (msg == NULL){
+    char *msg = get_log_message_string(max_lvl, level, location, message);
+    if (msg == NULL)
+    {
         // log level to low
         return;
     }
-    if (msg != NULL){
+    if (msg != NULL)
+    {
         logger_type type = logger->type;
-        char * file = logger->log_file;
+        char *file = logger->log_file;
 
-        if (type == LOGGER_TYPE_CONSOLE){
+        if (type == LOGGER_TYPE_CONSOLE)
+        {
             // log to console
             log_to_console(msg);
-        } else if (type == LOGGER_TYPE_FILE){
+        }
+        else if (type == LOGGER_TYPE_FILE)
+        {
             // log to file
             log_to_file(msg, file);
-        } else if (type == LOGGER_TYPE_BOTH){
+        }
+        else if (type == LOGGER_TYPE_BOTH)
+        {
             // log to console and file
             log_to_console(msg);
             log_to_file(msg, file);
         }
     }
+#else
+    UNUSED(logger);
+    UNUSED(level);
+    UNUSED(location);
+    UNUSED(format);
+#endif
 }
 
-void logger_log_if(struct logger_t * logger, int cond, log_level level, char * location, char * format, ...){
-    if (!cond){
+void logger_log_if(struct logger_t *logger, int cond, log_level level, char *location, char *format, ...)
+{
+#ifdef logging
+    if (!cond)
+    {
         // condition false -> nothing to log
         return;
     }
 
-    if (logger == NULL || logger->max_log_level == LOG_LEVEL_NONE){
+    if (logger == NULL || logger->max_log_level == LOG_LEVEL_NONE)
+    {
         return;
     }
 
@@ -178,23 +229,37 @@ void logger_log_if(struct logger_t * logger, int cond, log_level level, char * l
 
     log_level max_lvl = logger->max_log_level;
 
-    char * msg = get_log_message_string(max_lvl, level, location, message);
-    if (msg == NULL){
+    char *msg = get_log_message_string(max_lvl, level, location, message);
+    if (msg == NULL)
+    {
         // log level to low
         return;
     }
 
     // add message string to the write buffer
     fifo_push(logger->buffer, msg);
+#else
+    UNUSED(logger);
+    UNUSED(cond);
+    UNUSED(level);
+    UNUSED(location);
+    UNUSED(format);
+#endif
 }
 
-void logger_destroy(struct logger_t * logger){
+void logger_destroy(struct logger_t *logger)
+{
+#ifdef logging
 
     // free all remaining log messages
-    char * elem;
-    while ((elem = fifo_pop(logger->buffer)) != NULL) {
+    char *elem;
+    while ((elem = fifo_pop(logger->buffer)) != NULL)
+    {
         rfree(elem);
     }
 
     fifo_destroy(logger->buffer);
+#else
+    UNUSED(logger);
+#endif
 }
